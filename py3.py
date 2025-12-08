ca2b67b6899f7ca2c66830cb4c1b1c1f4dd72568
Nicky2424 <nandani2003k@gmail.com>
2025-12-08 16:39:11 +0530

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,9 +1,18 @@
 rows = int(input("Enter number of rows: "))
 
-for i in range(rows, 0, -1):
-    for j in range(0, i):
+# ----------- NORMAL PYRAMID -----------
+print("\nNormal Pyramid:")
+for i in range(1, rows + 1):
+    for j in range(i):
         print("*", end=" ")
-    
     print()
+
+# ----------- REVERSE PYRAMID -----------
+print("\nReverse Pyramid:")
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
     
     
