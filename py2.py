3194594525e807ba1221624d2b3263bbba7995a8
Nicky2424 <nandani2003k@gmail.com>
2025-12-08 16:42:28 +0530

    Update py2.py

--- py2.py
+++ py2.py
@@ -1,41 +1,26 @@
-# -------- 1) CHECK IF A NUMBER IS PRIME --------
-
-def check_prime(n):
-    if n <= 1:
-        return False
+def is_prime(n):
+    if n <= 1: return False
     for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
+        if n % i == 0: return False
     return True
 
-n = int(input("Enter a number: "))
+def primes_till(n):
+    return [i for i in range(2, n+1) if is_prime(i)]
 
-if check_prime(n):
-    print(n, "is a prime number")
-else:
-    print(n, "is NOT a prime number")
+def first_n_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num): primes.append(num)
+        num += 1
+    return primes
 
+N = int(input("Enter N: "))
 
-# -------- 2) PRINT ALL PRIME NUMBERS UP TO n --------
+print("\nA) Prime check:", "Prime" if is_prime(N) else "Not Prime")
+print("B) Primes till N:", *primes_till(N))
+print("C) First N primes:", *first_n_primes(N))
 
-print("\nPrime numbers up to", n, ":")
-
-for num in range(2, n + 1):
-    if check_prime(num):
-        print(num, end=" ")
-
-print("\n")
-
-
-# -------- 3) PRINT FIRST n PRIME NUMBERS --------
-
-print("First", n, "prime numbers:")
-
-count = 0
-num = 2
-
-while count < n:
-    if check_prime(num):
         print(num, end=" ")
         count += 1
     num += 1
