ac5d1f863b6d275dbd50db6cca2a6f3438bcf35d
Nicky2424 <nandani2003k@gmail.com>
2025-12-08 16:29:44 +0530

    Update py2.py

--- py2.py
+++ py2.py
@@ -1,47 +1,52 @@
-n = int(input("enter a number to check for prime or not"))
+
+# ----------- 1) CHECK IF A NUMBER IS PRIME -----------
+
+n = int(input("Enter a number to check if it is prime: "))
+
 if n <= 1:
-    print("not prime")
+    print(n, "is NOT a prime number")
 else:
-    is_prime = True  # Flag variable
-    for i in range(2, n + 1):
+    is_prime = True
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             is_prime = False
-            
-            print("not a prime no")
             break
-        else:
-            print("yes a prime no ")
-            break
+
+    if is_prime:
+        print(n, "is a prime number")
+    else:
+        print(n, "is NOT a prime number")
+
+
+# ----------- 2) PRINT ALL PRIME NUMBERS UP TO n -----------
 
 def is_prime(num):
-   if num <= 1:
-       return False
-   for i in range(2, int(num**0.5) + 1):
-       if num % i == 0:
-           return False
-   return True
-def print_primes(n):
-   for num in range(2, n + 1):
-       if is_prime(num):
-           print(num)
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-print_primes(n)
+print("\nPrime numbers up to", n, ":")
+for num in range(2, n + 1):
+    if is_prime(num):
+        print(num, end=" ")
 
-def print_prim(n):
-   if n < 2:
-       print("No prime numbers available.")
-       return
-   # Create a boolean array to mark prime numbers
-   primes = [True] * (n + 1)
-   primes[0] = primes[1] = False # 0 and 1 are not prime numbers
-   # Mark non-prime numbers
-   for i in range(2, int(n**0.5) + 1):
-       if primes[i]:
-           for j in range(i * i, n + 1, i):
-               primes[j] = False
-   # Print all prime numbers
-   for i in range(2, n + 1):
-       if primes[i]:
-           print(i, end=" ")
+print("\n")
 
-print_prim(n)
+
+# ----------- 3) PRINT FIRST n PRIME NUMBERS -----------
+
+def print_first_n_primes(count):
+    print("First", count, "prime numbers:")
+    num = 2
+    primes_found = 0
+
+    while primes_found < count:
+        if is_prime(num):
+            print(num, end=" ")
+            primes_found += 1
+        num += 1
+
+print_first_n_primes(n)
